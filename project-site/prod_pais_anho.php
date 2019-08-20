dddc9390891d6b8ee5cd362824cdd738bbd3f6eb
Stefano Faraggi <40745694+Stepp1@users.noreply.github.com>
2019-08-20 19:59:02 -0400

    Update prod_pais_anho.php

--- project-site/prod_pais_anho.php
+++ project-site/prod_pais_anho.php
@@ -7,19 +7,20 @@ try {
 
     $fuente = $_GET['input'];
 
-    $sql = 'Select p.name
-    FROM plant p, production prod
-    Where p.id_plant in (
-    Select q.id_plant
-    From location q
-    where q.latitude between 0 and 90)
-    and p.id_plant not in (
-    Select q.id_plant
-    from fuel q
-    where q.fuel = :fuel)
+    $sql = 'SELECT p.name,l1.country ,prod.electricity_generation
+    FROM plant p, production prod, location l1
+    WHERE p.id_plant IN (
+    SELECT q.id_plant
+    FROM location q
+    WHERE q.latitude BETWEEN 0 AND 90)
+    AND p.id_plant IN (
+    SELECT q.id_plant
+    FROM fuel q
+    WHERE q.fuel = :fuel)
     AND prod.id_plant = p.id_plant
-    order by prod.electricity_generation, p.name asc
-    LIMIT 50';
+    AND p.id_plant = l1.id_plant
+    ORDER BY prod.electricity_generation DESC
+    LIMIT 50;';
 
 
     $stmt = $pdo->prepare($sql);
